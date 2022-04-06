2419359a0506788f37be5eaa8f20a390f8774ffd
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-04-07 01:55:13 +0530

    Update BMS_sender.c

--- BMS_sender.c
+++ BMS_sender.c
@@ -1,16 +1,53 @@
 #include "BMS_sender.h"
 
-void printToConsole(char batteryData[])
+int TemperatureData[NUMBERS_OF_READINGS];
+int SOCData[NUMBERS_OF_READINGS];
+
+void printToConsole(int batteryData[])
 {
+	int i;
+	for (i = 0; i < NUMBERS_OF_READINGS; i++)
+	{
+		printf("%d\n", batteryData[i]);
+	}
 }
 
-void readDataFromFile(float* Temperature, float* SOC)
+
+bool readDataFromFile(FILE* filePtr, int sensorData[])
 {
+	if (NULL == filePtr)
+	{
+		printf("file cannot be opened \n");
+		return false;
+	}
+
+    int i;
+    for (i = 0; fscanf(filePtr, "%d", &sensorData[i]) != EOF; i++)
+    {
+    	//do nothing
+    }
+
+	fclose(filePtr);
+    printToConsole(sensorData);
+
+	return true;
 }
 
-void sendDataToConsole(float* Temperature, float* SOC)
+
+bool BMS_DataSender()
 {
-}
-void BMS_DataSender(void)
-{
+	bool retVal1, retVal2;
+	FILE* fptr;
+	fptr = fopen("temparatue_sensor.txt","r");
+	printf("Temperature Sensor Data\n");
+	retVal1 = readDataFromFile(fptr, TemperatureData);
+
+	printf("\nSOC Sensor Data\n");
+	fptr = fopen("soc_sensor.txt","r");
+	retVal2 = readDataFromFile(fptr, SOCData);
+
+	if(retVal1 && retVal2)
+		return true;
+	else
+		return false;
 }
