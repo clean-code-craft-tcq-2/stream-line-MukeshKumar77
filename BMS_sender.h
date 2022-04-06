268e82636b3a16be3cda727ce2bf9cbfac81f737
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-04-07 00:17:17 +0530

    Update BMS_sender.h

--- BMS_sender.h
+++ BMS_sender.h
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+
+#define NUMBERS_OF_READINGS    50
+
+void printToConsole(char batteryData[]);
+void readDataFromFile(float* Temperature, float* SOC);
+void sendDataToConsole(float* Temperature, float* SOC);
+void BMS_DataSender(void);
